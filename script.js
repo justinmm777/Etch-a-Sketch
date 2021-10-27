c50aa28a58dc88622ef8a412cf4bb2b47a3dda14
justinmm777 <justinmm777@gmail.com>
2021-10-27 12:53:16 +0100

    Limited user input between 1 to 100

--- script.js
+++ script.js
@@ -22,17 +22,37 @@ window.onload = makeGrid(16, 16);
 function gridBlack(){
     document.querySelectorAll('.columnBox').forEach(item => {
         item.addEventListener('mouseenter', event => {
-            item.style. backgroundColor = 'black'; 
+            item.style.backgroundColor = 'black'; 
         })
     })
 }
 gridBlack();
 
+// Generate random color
+function gridRandomColor(){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.querySelectorAll('.columnBox').forEach(item => {
+        item.addEventListener('mouseenter', event =>{
+            item.style.backgroundColor = "#" + randomColor;
+        })
+    })
+}
+
+const color = document.getElementById('btnColor');
+color.addEventListener('click', event => {
+    gridRandomColor();
+})
+
+
+
 // Event listener for clear button and getting user input
 const clear = document.getElementById('btnClear');
 
 clear.addEventListener('click', event => {
-    let num = Number(window.prompt("Enter number of squres"));
+    let num;
+    do {
+        num = Number(window.prompt("Enter number of squres"));
+    } while( num > 100 || num < 1)
     columnNum = num;
     rowNum = num;
     
